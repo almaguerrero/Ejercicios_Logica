b94470e884453a054d1deec217698c7063b3facc
AlmaGro <eliza_xeww@hotmail.com>
2023-06-28 12:06:17 -0600

    diez

--- ejercicio_10.js
+++ ejercicio_10.js
@@ -11,7 +11,21 @@ escalera(4) //devuelve
 [-][-][-][-]
 Como hacerlo:
 - Función que reciba un número
-- Recorrer el número de escaleras
+- Recorrer el número de niveles de la escaleras
 - En cada iteración pintar los escalones de ese nivel
 - Haciendo un bucle desde 1 hasta el nivel en el que estamos
 */
+function escalera(numero){
+  let escalera_completa = "";
+  for(let nivel = 1; nivel <= numero; nivel++){
+    let escalones = "";
+      for(let escalon = 1 ;escalon <= nivel;escalon++){
+        escalones += "[-]"
+
+      }
+      //concatenar
+      escalera_completa += escalones + '\n';
+  }
+  return escalera_completa;
+}
+console.log(escalera(15))
