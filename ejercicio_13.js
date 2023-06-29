31686480d6400b8f87f4233b3fef42e96847d2fa
AlmaGro <eliza_xeww@hotmail.com>
2023-06-29 06:32:21 -0600

    ejercicio 13

--- ejercicio_13.js
+++ ejercicio_13.js
@@ -1,4 +1,5 @@
 /*
+REPASAR
 Enunciado 13:
 Dado un array, dividirlo en tantos sub-arrays como sea necesario
 basandonos en un número que indique su tamaño. Dividirlo en arrays
@@ -12,6 +13,16 @@ Como hacerlo:
 - Crear un arrays
 - Recorrer array e ir añadiendo sub arrays
 */
-divideArray(array,numero){}
-
-divideArray([7,8,9,10,5],2)
+function divideArray(arreglo_principal,numero) {
+   let arreglos = [];
+   for(let elemento of arreglo_principal){
+     let ultimo = arreglos[arreglos.length - 1];
+    if(!ultimo || ultimo.length == numero){
+      arreglos.push([elemento])
+    }else{
+      ultimo.push(elemento)
+    }
+   }
+   return arreglos;
+}
+console.log(divideArray([7,8,9,10,5],2))
