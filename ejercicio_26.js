a0339b67aaa0880d5fc5fd4d37315a9a05afae82
AlmaGro <eliza_xeww@hotmail.com>
2023-07-04 08:51:25 -0600

    ejercicios 4 de julio

---
+++ ejercicio_26.js
@@ -0,0 +1,18 @@
+/*
+Dado un número, mostrar su serie fibonacci
+La serie fibonacci es una orden de números donde cada
+número es la suma de los dos anteriores
+
+Ejemplos:
+fib(10)[0]
+fib(10)[1]
+Serie completa: 0,1,1,2,3,5,8,13,21,34,55
+Resultado de la serie fibonacci es 55
+
+Como hacerlo:
+- Crear una función con parámetro numero
+- Crear variable con dos primeros numeros de la Serie
+- Bucle desde dos al número
+- Sumar los dos anteriores valores a numero actual
+- Devolvre resultado
+*/
